23fe8f936cb1f363c000cbb276107f48122c2165
Nick Belemet <n.belemet@gmail.com>
2026-07-16 12:41:38 +0100

    Setting up pygame environment

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('Ready to play Tic-Tac-Toe!')
+import pygame, sys
+
+# Initialize Pygame
+pygame.init()
+
+screen = pygame.display.set_mode((300, 600))
+pygame.display.set_caption("Tetris")
+
+clock = pygame.time.Clock()
+
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            pygame.quit()
+            sys.exit() 
+
+    pygame.display.update()
+    clock.tick(60)  # Limit the frame rate to 60 frames per second
